b1c9cf3fbda31f00014cf3c55f5e309edaa25dbd
Mirza Ardani <39607439+mirzaardani@users.noreply.github.com>
2019-05-30 17:01:53 +0700

    init

---
+++ src/main/java/com/latihanmockito/latihanmockito/LatihanmockitoApplication.java
@@ -0,0 +1,13 @@
+package com.latihanmockito.latihanmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanmockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanmockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/latihanmockito/latihanmockito/LatihanmockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latihanmockito.latihanmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanmockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
